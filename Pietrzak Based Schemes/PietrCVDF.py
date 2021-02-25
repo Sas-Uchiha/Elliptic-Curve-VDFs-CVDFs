e9f8952772a42b8806f46bd320501ba73166c12d
Sas-Uchiha <79395979+Sas-Uchiha@users.noreply.github.com>
2021-02-25 10:04:09 +0530

    Update PietrCVDF.py

--- Pietrzak Based Schemes/PietrCVDF.py
+++ Pietrzak Based Schemes/PietrCVDF.py
@@ -1,3 +1,4 @@
+'''Input: Command Line Inputs: T, the delay parameter followed by t: time upto which the cVDF needs to be calculated'''
 import Pietr as pietr
 import EC as elliptic_curve
 import sys
